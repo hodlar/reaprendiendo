48018ed12321f9ccb5811003357fb23766b73713
Carlos Rodri­guez <carlosrdz.isd@gmail.com>
2023-03-04 19:28:01 -0600

    rbterminado

--- learningC/data_structures/rb_tree/rb_tree.c
+++ learningC/data_structures/rb_tree/rb_tree.c
@@ -10,18 +10,148 @@ void create_tree (Node **root, int data)
 	(*root)->right = 0;
 }
 
-void rebalance(Node *root, Node *actual_node)
+
+void rotate_left (Node **root, Node *actual_node)
 {
-    if( (actual_node->parent)->color == 0 )
+    Node *x, *y;
+    x = actual_node->parent;
+    y = actual_node;
+    
+    x->right = y->left;
+    if(y->left)
+    { (y->left)->parent = x; }
+    
+    if( x->parent == 0 )
     {
-        
+        y->parent = 0;
+        *root = y;
     }
+    else if ( (x->parent)->left == x )
+    {
+        y->parent = x->parent;
+        (x->parent)->left = y;
+    }
+    else{
+        y->parent = x->parent;
+        (x->parent)->right = y;
+    }
+    y->left = x;
+    x->parent = y;
 }
 
-Node insert_leaf (Node *root, int data)
+
+void rotate_right (Node **root, Node *actual_node)
+{
+    Node *x, *y;
+    x = actual_node->parent;
+    y = actual_node;
+    
+    y->left = x->right;
+    if(x->right)
+    { (x->right)->parent = y; }
+    
+    if( y->parent == 0 )
+    {
+        x->parent = 0;
+        *root = x;
+    }
+    else if ( (y->parent)->right == y )
+    {
+        x->parent = y->parent;
+        (y->parent)->right = x;
+    }
+    else{
+        x->parent = y->parent;
+        (y->parent)->left = x;
+    }
+    x->left = y;
+    y->parent = x;
+}
+
+void rotate_left_right(Node **root, Node *actual_node)
+{
+    Node *y;
+    y = actual_node;
+    rotate_left(root,y);
+    rotate_right(root,y);
+}
+
+void rotate_right_left(Node **root, Node *actual_node)
+{
+    Node *x;
+    x = actual_node;
+    rotate_right(root,x);
+    rotate_left(root,x);
+
+}
+
+int recolor (Node *root, Node *actual_node)
+{
+    return 0;
+}
+
+void rebalance(Node **root, Node *actual_node)
+{
+    Node *new_node, *p, *gP, *z, *x;
+    new_node = actual_node;
+    z = 0;
+    x = 0;
+    p = new_node->parent;
+    gP = p->parent;
+    if(gP->right)
+        if( (gP->right)->left )
+        { z = (gP->right)->left; }
+        else if( (gP->right)->right )
+        { z = (gP->right)->right; }
+        else 
+        { z = 0; }
+    
+    while( p->color == 0 )
+    {
+        if (gP->left == p && z)
+        {
+            if( (gP->right)->color == 0)
+            {  
+                (gP->right)->color = 1;
+                (gP->left)->color = 1;
+                gP->color = 0;
+                new_node = gP;
+            }
+            else if(new_node == p->right)
+            {
+                new_node = p;
+                rotate_left(root, new_node);
+            }
+            else
+            {
+                p->color = 1;
+                gP->color = 0;
+                rotate_right(root,gP);
+            }
+        }
+        if( (gP->left)->color == 0 )
+        {
+            (gP->left)->color = 1;
+            (gP->right)->color = 1;
+            gP->color = 0;
+            new_node = gP;
+        }
+        else if(new_node == p->left ){
+            new_node = p;
+            rotate_right(root, new_node);
+            p->color = 1;
+            gP->color = 0;
+            rotate_left(root, gP);
+        }
+    }
+    (*root)->color = 1;
+}
+
+
+void insert_leaf (Node **root, int data)
 {
     Node *tmp;
-    tmp = root;
+    tmp = *root;
     
     if( tmp->data < data )
     {
@@ -29,9 +159,9 @@ Node insert_leaf (Node *root, int data)
         {
             tmp->right = create_Node(data);
             (tmp->right)->parent = tmp;
-            rebalance(tmp->right);
+            rebalance(root, tmp->right);
         }
-        else { insert_leaf( tmp->right, data ); }
+        else { insert_leaf( &tmp->right, data ); }
     }
     else
     {
@@ -40,25 +170,10 @@ Node insert_leaf (Node *root, int data)
             tmp->left = create_Node(data);
             (tmp->left)->parent = tmp;
         }
-        else { insert_leaf( tmp->left, data ); }
+        else { insert_leaf( &tmp->left, data ); }
     }
 }
-/*
-int rotate_left (Node *root, Node *actual_node)
-{
-    return 0;
-}
 
-int rotate_right (Node *root, Node *actual_node)
-{
-    return 0;
-}
-
-int recolor (Node *root, Node *actual_node)
-{
-    return 0;
-}
-*/
 void print (Node *root)
 {
     Node *tmp;
--- learningC/data_structures/rb_tree/rb_tree.h
+++ learningC/data_structures/rb_tree/rb_tree.h
@@ -1,10 +1,11 @@
 #include "node.h"
 
 void create_tree (Node **root, int data);
-void rebalance(Node *root, Node *actual_node);
-void insert_leaf (Node *root, int data);
-int rotate_left (Node *root, Node *actual_node);
-int rotate_right (Node *root, Node *actual_node);
+void rebalance(Node **root, Node *actual_node);
+void insert_leaf (Node **root, int data);
+void rotate_left (Node **root, Node *actual_node);
+void rotate_right (Node **root, Node *actual_node);
+void rotate_left_right(Node **root, Node *actual_node);
 int recolor (Node *root, Node *actual_node);
 void print (Node *root);
 
