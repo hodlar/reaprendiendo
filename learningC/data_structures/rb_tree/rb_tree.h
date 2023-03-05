baa63faef09e97c919da5bd96078c98bf7549288
Carlos Rodri­guez <carlosrdz.isd@gmail.com>
2023-03-04 20:48:52 -0600

    header terminado

--- learningC/data_structures/rb_tree/rb_tree.h
+++ learningC/data_structures/rb_tree/rb_tree.h
@@ -2,10 +2,11 @@
 
 void create_tree (Node **root, int data);
 void rebalance(Node **root, Node *actual_node);
-void insert_leaf (Node **root, int data);
 void rotate_left (Node **root, Node *actual_node);
 void rotate_right (Node **root, Node *actual_node);
 void rotate_left_right(Node **root, Node *actual_node);
+void rotate_right_left(Node **root, Node *actual_node);
 int recolor (Node *root, Node *actual_node);
+void insert_leaf (Node **root, int data);
 void print (Node *root);
 
