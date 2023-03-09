67750e9f0709584d9aa33d4334508f8ff97c5487
Carlos Rodri­guez <carlosrdz.isd@gmail.com>
2023-03-09 14:09:42 -0600

    lectura corregida

--- learningCpp/rolando/main.cpp
+++ learningCpp/rolando/main.cpp
@@ -13,7 +13,7 @@ int main()
     ifstream myfile;
     Hora unaH;
     int tmp_int;
-    string tmp_str;
+    string tmp_str, renglon;
     vector<Funcion> funV;
     vector<Pelicula> peliV;
     vector<Actor> actV;
@@ -26,14 +26,19 @@ int main()
     cerr << "Error in opening the file" << endl;
     }
     
-    while (myfile >> tmp_int >> tmp_str) {
+    while ( getline(myfile >> ws, renglon) ) {
+        tmp_int = stoi(renglon.substr(0,2) );
+        tmp_str = renglon.erase(0,2);
         act.set_id(tmp_int);
         act.set_nombre(tmp_str);
         actV.push_back(act);
     }
     myfile.close();
     
-    myfile.open("Peliculas.txt");
+    for(int k=0; k < 10; k++)
+    {
+        cout << actV[k].get_id() << actV[k].get_nombre() << endl;
+    }
 
     
     
